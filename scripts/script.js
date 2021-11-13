d72e5d9671e10dd40baa9ebd88da083631e12967
Kirill Tsurkan <tsurkan2019@yandex.ru>
2021-11-13 21:37:42 +0300

    feat: add like function

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,61 @@
-const popupElement = document.querySelector('.popup');
+const popupElements = document.querySelectorAll('.popup');
 const closePopup = document.querySelector('.popup__close-button');
 const openPopup = document.querySelector('.profile__edit-button');
 const saveButton = document.querySelector('.form__save-button');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__description');
-let formElement = document.querySelector('.form')// Воспользуйтесь методом querySelector()
+const formElement = document.querySelector('.form');
+const addButton = document.querySelector('.profile__add-button');
+const LikeButtonElements = document.querySelectorAll('.cards__like');
 // Находим поля формы в DOM
-let nameInput = document.querySelector('.form__input_type_name') // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.form__input_type_job') // Воспользуйтесь инструментом .querySelector()
+let nameInput = document.querySelector('.form__input_type_name')
+let jobInput = document.querySelector('.form__input_type_job')
+let linkInput = document.querySelector('.form__input_type_link')
 
 
-
+//функции открытия и закрытия
 function viewPopup() {
-  popupElement.classList.add('popup_opened');
+  popupElements.forEach(function(popup)  {
+    classList.add('popup_opened');
+//  if ()
+});
+};
+function openTypeProfileModal() {
+
   jobInput.value = profileJob.textContent;
   nameInput.value = profileName.textContent;
 }
 
+
 function noViewPopup () {
   popupElement.classList.remove('popup_opened');
-
 }
 
+//function inputProfile() {
+ // viewPopup();
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+//}
+
+
+
+
+// Обработчик «отправки» формы.
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileJob.textContent = jobInput.value;
   profileName.textContent = nameInput.value;
   noViewPopup();
 
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
 }
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 openPopup.addEventListener('click', viewPopup);
 closePopup.addEventListener('click', noViewPopup);
+
+LikeButtonElements.forEach(function (like) {
+  like.addEventListener('click', (event) => {
+  event.target.classList.toggle('cards__like_aktive');
+  });
+});
 
